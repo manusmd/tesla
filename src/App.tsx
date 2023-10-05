f2793a8eed41cdce46fc2da32f10aa9ff9cdd449
Manuel Schmid <manuel.schmid@js-tut.de>
2023-10-05 22:49:00 +0200

    Implement snap scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import Navigation from "./components/Navigation/Navigation.tsx";
-import {useState} from "react";
+import { useState} from "react";
 import {ProductView} from "./components/ProductView/ProductView.tsx";
+import styles from "./App.module.css";
 
 function App() {
 const [textBlack, _setTextBlack] = useState(false)
-  return (
-    <>
+    return (
+    <div className={styles.container}>
         <Navigation textBlack={textBlack} />
-        <ProductView video={"/Model3Hero.mp4"} textBlack={textBlack} title={"Model 3"} pricing={"From $29,740*"} info={"After Federal Tax Credit & Est. Gas Savings"}/>
-        <ProductView image={"/ModelY.avif"} textBlack={true} title={"Model Y"} pricing={"From $39,390*"} info={"After Federal Tax Credit & Est. Gas Savings"}/>
-    </>
+        <div className={styles.productScroll}>
+            <ProductView video={"/Model3Hero.mp4"} textBlack={textBlack} title={"Model 3"} pricing={"From $29,740*"} info={"After Federal Tax Credit & Est. Gas Savings"} gradient/>
+            <ProductView image={"/ModelY.avif"} textBlack={true} title={"Model Y"} pricing={"From $39,390*"} info={"After Federal Tax Credit & Est. Gas Savings"}/>
+        </div>
+    </div>
   )
 }
 
